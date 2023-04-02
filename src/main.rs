d8684b23d4eef77d4a73104f4c767ba5fbe674c7
Jan-Loettgen <janlucaloettgen@gmail.com>
2023-04-02 23:01:19 +0100

    partial solution to day 7

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ pub mod solutions;
 
 fn main() {
     println!("Hello, world!");
-    solutions::day_1::solve(1);
-    solutions::day_1::solve(2);
-    solutions::day_2::solve(1);
-    solutions::day_2::solve(2);
-    solutions::day_3::solve(1);
-    solutions::day_3::solve(2);
-    solutions::day_4::solve(1);
-    solutions::day_4::solve(2);
-    solutions::day_5::solve(1);
-    solutions::day_5::solve(2);
-    solutions::day_6::solve(1);
-    solutions::day_6::solve(2);
+    // solutions::day_1::solve(1);
+    // solutions::day_1::solve(2);
+    // solutions::day_2::solve(1);
+    // solutions::day_2::solve(2);
+    // solutions::day_3::solve(1);
+    // solutions::day_3::solve(2);
+    // solutions::day_4::solve(1);
+    // solutions::day_4::solve(2);
+    // solutions::day_5::solve(1);
+    // solutions::day_5::solve(2);
+    // solutions::day_6::solve(1);
+    // solutions::day_6::solve(2);
+    solutions::day_7::solve(1);
+    // solutions::day_6::solve(2);
 }
